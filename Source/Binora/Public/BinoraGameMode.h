f288bac1330233c70ab65762ba28a6c6dcd78f1e
Matheus Vilano <matheusvilano@icloud.com>
2021-04-22 06:29:22 -0700

    Properly formatted BinoraGameMode header

--- Source/Binora/Public/BinoraGameMode.h
+++ Source/Binora/Public/BinoraGameMode.h
@@ -75,9 +75,8 @@ class BINORA_API ABinoraGameMode : public AGameModeBase
 		protected:
 
 		    // Handle to be used by the Time Manager
-        	UPROPERTY();
+        	UPROPERTY()
 			FTimerHandle MemorizationTimerHandle;
-
 
 			// This should run every second.
 			UFUNCTION()
